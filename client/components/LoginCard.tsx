688c8e1bf3958b50a4419571c212af96bd0a3c6a
FGibeau <frederick.gibeau@gmail.com>
2020-04-09 07:56:15 -0400

    Add login card component

---
+++ client/components/LoginCard.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import {
+  Card,
+  CardContent,
+  Typography,
+  CardActions,
+  Button,
+} from '@material-ui/core';
+
+export default function LoginCard() {
+  return (
+    <Card>
+      <CardContent>
+        <Typography>Boo</Typography>
+      </CardContent>
+      <CardActions>
+        <Button variant="contained" color="primary">
+          Brace Yourself!
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
--- client/index.tsx
+++ client/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {
-  Button,
   createMuiTheme,
   ThemeProvider,
   CssBaseline,
   Typography,
   AppBar,
 } from '@material-ui/core';
+import LoginCard from './components/LoginCard';
 
 const theme = createMuiTheme({
   palette: {
@@ -19,12 +19,7 @@ const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <AppBar>
-        <Typography variant="h6">Anodin Online</Typography>
-      </AppBar>
-      <Typography style={{ marginTop: 50 }}>
-        On lâche pas l&apos;progrès... !
-      </Typography>
+      <LoginCard />
     </ThemeProvider>
   );
 };
