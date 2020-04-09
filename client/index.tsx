2f0aff21846aa2e4460147e6b5e685f75872df81
FGibeau <frederick.gibeau@gmail.com>
2020-04-09 06:58:53 -0400

    Integrate material UI

--- client/index.tsx
+++ client/index.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Button } from '@material-ui/core';
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById('root'));
+ReactDOM.render(
+  <Button variant="contained" color="primary">
+    Ben oui, un beau boutton !
+  </Button>,
+  document.getElementById('root')
+);
